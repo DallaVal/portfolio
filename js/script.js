e18c4c349233ad9874c44844def641b3528f5365
DallaVal <matheusgabrieldalla@gmail.com>
2025-02-19 16:46:29 -0300

    publicação com git

---
+++ js/script.js
@@ -0,0 +1,103 @@
+
+        function buttomentrar(){
+            var buttom1 = document.getElementById('buttom-bggreen')
+            buttom1.innerText = 'não funciona'
+            buttom1.style.background = 'red'
+        }
+
+        function buttomclicar(){
+            var buttom2 = document.getElementById('buttom-bggreen')
+            buttom2.innerText = 'se é tonto?'
+            buttom2.style.background = 'red'
+        }
+
+        function buttomsair(){
+            var buttom1 = document.getElementById('buttom-bggreen')
+            buttom1.innerText = 'Download CV'
+            buttom1.style.background = '#00DF5E'
+        }
+
+        function buttomentrar2(){
+            var buttom1 = document.getElementById('buttom-bgblack')
+            buttom1.innerText = 'não funciona'
+            buttom1.style.background = 'red'
+        }
+
+        function buttomclicar2(){
+            var buttom2 = document.getElementById('buttom-bgblack')
+            buttom2.innerText = 'fez curso?'
+            buttom2.style.background = 'red'
+        }
+
+        function buttomsair2(){
+            var buttom1 = document.getElementById('buttom-bgblack')
+            buttom1.innerText = 'Entrar em contato'
+            buttom1.style.background = 'transparent'
+        }
+
+        function entrar(){
+            var imagem = document.getElementById('imagem-emconstrucao')
+            imagem.src = './img/embreve.webp'
+            var div = document.getElementById('projeto-4')
+            div.style.background = 'red'
+            var img = document.getElementById('img-emconstrucao')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao')
+            h2.innerText = 'Em andamento'
+        }
+
+        function sair(){
+            var imagem = document.getElementById('imagem-emconstrucao')
+            imagem.src = './img/imagem-projetoemandamento.jpeg'
+            var div = document.getElementById('projeto-4')
+            div.style.background = '#333'
+            var img = document.getElementById('img-emconstrucao')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao')
+            h2.innerText = 'Em breve'
+        }
+
+        function entrar2(){
+            var imagem = document.getElementById('imagem-emconstrucao2')
+            imagem.src = './img/embreve.webp'
+            var div = document.getElementById('projeto-5')
+            div.style.background = 'red'
+            var img = document.getElementById('img-emconstrucao2')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao2')
+            h2.innerText = 'Em andamento'
+        }
+
+        function sair2(){
+            var imagem = document.getElementById('imagem-emconstrucao2')
+            imagem.src = './img/imagem-projetoemandamento.jpeg'
+            var div = document.getElementById('projeto-5')
+            div.style.background = '#333'
+            var img = document.getElementById('img-emconstrucao2')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao2')
+            h2.innerText = 'Em breve'
+        }
+
+        function entrar3(){
+            var imagem = document.getElementById('imagem-emconstrucao3')
+            imagem.src = './img/embreve.webp'
+            var div = document.getElementById('projeto-6')
+            div.style.background = 'red'
+            var img = document.getElementById('img-emconstrucao3')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao3')
+            h2.innerText = 'Em andamento'
+        }
+
+        function sair3(){
+            var imagem = document.getElementById('imagem-emconstrucao3')
+            imagem.src = './img/imagem-projetoemandamento.jpeg'
+            var div = document.getElementById('projeto-6')
+            div.style.background = '#333'
+            var img = document.getElementById('img-emconstrucao3')
+            img.style.background = 'white'
+            var h2 = document.getElementById('h2-emconstrucao3')
+            h2.innerText = 'Em breve'
+        }
+
